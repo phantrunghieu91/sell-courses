f090017c9be49914acb821cbfabcc33d54817b19
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-07-15 17:35:37 +0700

    UPDATE: Register - Registered style for home page, header

--- inc/base/Register.php
+++ inc/base/Register.php
@@ -62,12 +62,12 @@ class Register extends BaseController {
     $this->enqueueStyle( 'theme-init', time() );
     $this->enqueueStyle( 'google-symbols', null, 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200' );
 
-    $this->enqueueStyle( 'gpw-footer', time() );
+    $this->enqueueStyle( 'jins-header', time() );
+    $this->enqueueStyle( 'jins-footer', time() );
 
     // * Enqueue swiper for page that needs it
     if( is_front_page() ) {
-      $this->enqueueScript( 'swiper', '12.2.0' );
-      $this->enqueueStyle( 'swiper', '12.2.0' );
+      $this->enqueueStyle( 'jins-home-page', time() );
     }
   }
   public function setTypeForModuleScripts() {
