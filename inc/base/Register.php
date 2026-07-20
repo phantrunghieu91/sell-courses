9f3981ec10cbb06fa62078b0962e206604b79199
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-07-20 17:12:57 +0700

    UPDATE: Contact page - Created contact page template - Added hero section + styled

--- inc/base/Register.php
+++ inc/base/Register.php
@@ -70,6 +70,10 @@ class Register extends BaseController {
       $this->enqueueScript( 'jins-home-page', time() );
       $this->enqueueStyle( 'jins-home-page', time() );
     }
+
+    if( is_page( [ 45 ] ) ) {
+      $this->enqueueStyle( 'jins-contact-page', time() );
+    }
   }
   public function setTypeForModuleScripts() {
     if( empty( $this->module_scripts ) ) {
---
+++ page-contact-template.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author Hieu "Jin" Phan Trung
+ * * Template name: JINS PAGE - Contact page
+ */
+get_template_part( 'gpw-templates/global/header' );
+
+get_template_part( 'gpw-templates/global/hero-section', null, [ 'display_breadcrumbs' => true ] );
+
+get_template_part( 'gpw-templates/global/footer' );
