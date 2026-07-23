82314cd6e80fa8d8a9451357d453486308208b63
Hieu Phan Trung <phantrunghieu1291@gmail.com>
2026-07-23 11:10:25 +0700

    FIXED: Global swiper template - Changed navigation button class name

--- gpw-templates/global/swiper-template.php
+++ gpw-templates/global/swiper-template.php
@@ -3,34 +3,34 @@
  * @package GiapPhapWeb_Theme
  * * Template render swiper slide
  */
-$slideItems = $args['slide_items'] ?? [];
-$hasNav = $args['has_nav'] ?? false;
+$slideItems    = $args['slide_items']       ?? [];
+$hasNav        = $args['has_nav']               ?? false;
 $hasPagination = $args['has_pagination'] ?? false;
-$hasScrollbar = $args['has_scrollbar'] ?? false;
-if( !isset($slideItems) || empty($slideItems)) {
+$hasScrollbar  = $args['has_scrollbar']   ?? false;
+if( !isset( $slideItems ) || empty( $slideItems ) ) {
   return;
 }
 ?>
 <div class="swiper">
   <div class="swiper-wrapper">
-    <?php foreach($slideItems as $slideItem) : ?>
+    <?php foreach( $slideItems as $slideItem ) : ?>
       <div class="swiper-slide">
         <?= $slideItem ?>
       </div>
     <?php endforeach; ?>
   </div>
-  <?php if($hasNav) : ?>
-    <a class="gpw-nav-btn gpw-nav-btn__prev" role="button" aria-label="Previous slide">
+  <?php if( $hasNav ) : ?>
+    <a class="jins-swiper-nav-btn jins-swiper-nav-btn__prev" role="button" aria-label="Previous slide">
       <span class="material-symbols-outlined">chevron_left</span>
     </a>
-    <a class="gpw-nav-btn gpw-nav-btn__next" role="button" aria-label="Next slide">
+    <a class="jins-swiper-nav-btn jins-swiper-nav-btn__next" role="button" aria-label="Next slide">
       <span class="material-symbols-outlined">chevron_right</span>
     </a>
   <?php endif; ?>
-  <?php if($hasPagination) : ?>
+  <?php if( $hasPagination ) : ?>
     <div class="gpw-pagination"></div>
   <?php endif; ?>
-  <?php if($hasScrollbar) : ?>
+  <?php if( $hasScrollbar ) : ?>
     <div class="swiper-scrollbar"></div>
   <?php endif; ?>
 </div>
